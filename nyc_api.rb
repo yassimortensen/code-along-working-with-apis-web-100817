6a2a90a96ac71db858d16e10d84eb1bd3d06b466
yassimortensen <yassimortensen@gmail.com>
2017-10-13 15:42:47 -0400

    finished code along

--- nyc_api.rb
+++ nyc_api.rb
@@ -1,7 +1,7 @@
 require 'net/http'
  require 'open-uri'
  require 'json'
- 
+
  class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -19,7 +19,17 @@ require 'net/http'
     end
   end
 
+  def program_school
+     # we use the JSON library to parse the API response into nicely formatted JSON
+     programs = JSON.parse(self.get_programs)
+     programs.collect do |program|
+       program["agency"]
+     end
+  end
+
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+ # programs = GetPrograms.new.get_programs
+ # puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
